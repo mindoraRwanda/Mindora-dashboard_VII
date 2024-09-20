51483bbd3979b5da1c0335678e39dc4ce714cdbf
ikundabayoplacide <ikundabayoplacide500@gmail.com>
2024-09-20 22:10:47 +0200

    Login with API

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -18,7 +18,7 @@ const Login = () => {
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
-      navigate('/dashboard');
+      // navigate('/dashboard');
       try {
         const resultAction = await dispatch(loginUser({ email, password }));
         if (loginUser.fulfilled.match(resultAction)) {
